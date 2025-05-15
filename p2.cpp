2cc271a7b5c89693f7c7222bdceb730961e80eb5
ykn <66902257+y2k3n@users.noreply.github.com>
2025-05-15 13:02:53 +0800

    Fix includes.

--- p2.cpp
+++ p2.cpp
@@ -1,15 +1,15 @@
+#include "llvm/IR/Argument.h"
 #include "llvm/IR/Function.h"
 #include "llvm/IR/Instruction.h"
 #include "llvm/IR/Instructions.h"
 #include "llvm/IR/Module.h"
+#include "llvm/IR/User.h"
 #include "llvm/IR/Value.h"
 #include "llvm/IRReader/IRReader.h"
 #include "llvm/Support/InitLLVM.h"
 #include "llvm/Support/SourceMgr.h"
 #include "llvm/Support/raw_ostream.h"
 
-#include <llvm-19/llvm/IR/Argument.h>
-#include <llvm-19/llvm/IR/User.h>
 #include <queue>
 #include <set>
 #include <unordered_map>
