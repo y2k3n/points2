3b2007db90d909e2a9ec84b26a3d1dd425d1cb61
ykn <66902257+y2k3n@users.noreply.github.com>
2025-05-18 20:46:01 +0800

    Add csv stats output.

--- compile.sh
+++ compile.sh
@@ -1,7 +1,11 @@
-clang++ -O3 p2-steensgaard.cpp `llvm-config --cxxflags --ldflags --system-libs --libs core` -o p2-steensgaard
+# clang++ -O3 p2-steensgaard.cpp `llvm-config --cxxflags --ldflags --system-libs --libs core` -o p2-steensgaard
 
-clang++ -O3 -g p2-inter-dense.cpp `llvm-config --cxxflags --ldflags --system-libs --libs core` -o p2-inter-dense
+# clang++ -O3 -g p2-inter-dense.cpp `llvm-config --cxxflags --ldflags --system-libs --libs core` -o p2-inter-dense
 
 # clang++ -O3 -g p2-inter.cpp `llvm-config --cxxflags --ldflags --system-libs --libs core` -o p2-inter
 
 clang++ -O3 p2.cpp `llvm-config --cxxflags --ldflags --system-libs --libs core` -o p2
+
+clang++ -O3 p2.cpp -DCONCURRENT -DNTHREADS=4 -DPRINT_STATS `llvm-config --cxxflags --ldflags --system-libs --libs core` -o p2-c
+
+clang++ -O3 p2.cpp -DCSV -DRUN_COUNT=3 `llvm-config --cxxflags --ldflags --system-libs --libs core` -o p2-csv
--- p2.cpp
+++ p2.cpp
@@ -10,18 +10,39 @@
 #include "llvm/Support/SourceMgr.h"
 #include "llvm/Support/raw_ostream.h"
 
+#include <chrono>
+#include <mutex>
 #include <queue>
 #include <set>
+#include <thread>
 #include <unordered_map>
-#include <chrono>
+#include <cmath>
+#include <string>
+#include <fstream>
 
 using namespace llvm;
 
-std::unordered_map<Value *, std::set<Value *>> pt;
-std::queue<std::pair<Value *, std::set<Value *>>> worklist;
-std::unordered_map<Value *, std::set<Value *>> PFG;
 
-void addEdge(Value *s, Value *t) {
+std::mutex outsmtx;
+
+struct TaskInfo {
+  Function *func;
+  size_t size;
+  int index;
+
+  bool operator<(const TaskInfo &rhs) const { return size < rhs.size; }
+};
+
+struct LocalData {
+  std::unordered_map<Value *, std::set<Value *>> pt;
+  std::queue<std::pair<Value *, std::set<Value *>>> worklist;
+  std::unordered_map<Value *, std::set<Value *>> PFG;
+};
+
+void addEdge(Value *s, Value *t, LocalData &localdata) {
+  auto& pt = localdata.pt;
+  auto &worklist = localdata.worklist;
+  auto& PFG = localdata.PFG;
   if (PFG[s].find(t) == PFG[s].end()) {
     PFG[s].insert(t);
     if (!pt[s].empty()) {
@@ -30,7 +51,10 @@ void addEdge(Value *s, Value *t) {
   }
 }
 
-void propagate(Value *n, const std::set<Value *> &pts) {
+void propagate(Value *n, const std::set<Value *> &pts, LocalData &localdata) {
+  auto &pt = localdata.pt;
+  auto &worklist = localdata.worklist;
+  auto &PFG = localdata.PFG;
   if (!pts.empty()) {
     pt[n].insert(pts.begin(), pts.end());
     for (auto *s : PFG[n]) {
@@ -39,7 +63,8 @@ void propagate(Value *n, const std::set<Value *> &pts) {
   }
 }
 
-void initialize(Function &func) {
+void initialize(Function &func, LocalData& localdata) {
+  auto &worklist = localdata.worklist;
   for (auto &BB : func) {
     for (auto &inst : BB) {
 
@@ -53,7 +78,7 @@ void initialize(Function &func) {
         for (int i = 0; i < phi->getNumIncomingValues(); ++i) {
           Value *val = phi->getIncomingValue(i);
           if (isa<Instruction>(val) || isa<Argument>(val)) {
-            addEdge(val, phi);
+            addEdge(val, phi, localdata);
           }
         }
 
@@ -61,23 +86,25 @@ void initialize(Function &func) {
         Value *tval = select->getTrueValue();
         Value *fval = select->getFalseValue();
         if (isa<Instruction>(tval) || isa<Argument>(tval)) {
-          addEdge(tval, select);
+          addEdge(tval, select, localdata);
         }
         if (isa<Instruction>(fval) || isa<Argument>(fval)) {
-          addEdge(fval, select);
+          addEdge(fval, select, localdata);
         }
 
       } else if (auto *cast = dyn_cast<CastInst>(&inst)) {
         Value *src = cast->getOperand(0);
-        addEdge(src, cast);
+        addEdge(src, cast, localdata);
       }
-
       // iter end
     }
   }
 }
 
-void solve() {
+void solve(LocalData& localdata) {
+  auto &pt = localdata.pt;
+  auto &worklist = localdata.worklist;
+  // auto &PFG = localdata.PFG;
   while (!worklist.empty()) {
     auto [n, pts] = worklist.front();
     worklist.pop();
@@ -85,7 +112,7 @@ void solve() {
     std::set<Value *> delta;
     std::set_difference(pts.begin(), pts.end(), pt[n].begin(), pt[n].end(),
                         std::inserter(delta, delta.begin()));
-    propagate(n, delta);
+    propagate(n, delta, localdata);
 
     for (auto *user : n->users()) {
       if (StoreInst *store = dyn_cast<StoreInst>(user)) {
@@ -94,7 +121,7 @@ void solve() {
           Value *y = store->getValueOperand();
           if (isa<Instruction>(y) || isa<Argument>(y)) {
             for (Value *oi : delta) {
-              addEdge(y, oi);
+              addEdge(y, oi, localdata);
             }
           }
         }
@@ -104,7 +131,7 @@ void solve() {
         if (load->getPointerOperand() == n) {
           Value *y = load;
           for (Value *oi : delta) {
-            addEdge(oi, y);
+            addEdge(oi, y, localdata);
           }
         }
       }
@@ -113,7 +140,9 @@ void solve() {
   }
 }
 
-void print() {
+void print(LocalData& localdata) {
+  auto &pt = localdata.pt;
+  auto &PFG = localdata.PFG;
   outs() << "Points-to Set:\n";
   outs() << "=================\n";
   for (auto &[p, points2] : pt) {
@@ -133,6 +162,80 @@ void print() {
   //   }
   //   outs() << "\n";
   // }
+}
+
+void threadedPoints2(std::mutex &Qmutex, std::priority_queue<TaskInfo> &taskQ,
+                      int tid) {
+  auto start = std::chrono::high_resolution_clock::now();
+  int max_time = 0;
+  int max_size = 0;
+  int task_count = 0;
+  int total_size = 0;
+  int total_size_sq = 0;
+  int total_time = 0;
+  int total_time_sq = 0;
+
+  while (true) {
+    int index;
+    Function *func;
+    int size;
+    {
+      std::lock_guard<std::mutex> lock(Qmutex);
+      if (taskQ.empty())
+        break;
+      index = taskQ.top().index;
+      func = taskQ.top().func;
+      size = taskQ.top().size;
+      taskQ.pop();
+    }
+#ifdef PRINT_STATS
+    auto sub_start = std::chrono::high_resolution_clock::now();
+#endif
+
+    LocalData localdata;
+    initialize(*func, localdata);
+    solve(localdata);
+
+#ifdef PRINT_STATS
+    auto sub_end = std::chrono::high_resolution_clock::now();
+    auto sub_duration =
+        std::chrono::duration_cast<std::chrono::milliseconds>(sub_end -
+        sub_start);
+    int time = sub_duration.count();
+    if (time > max_time){
+      max_time = time;
+      max_size = size;
+    }
+    task_count++;
+    total_size += size;
+    total_size_sq += size * size;
+    total_time += time;
+    total_time_sq += time * time;
+#endif
+  }
+
+#ifdef PRINT_STATS
+  auto end = std::chrono::high_resolution_clock::now();
+  auto duration =
+      std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
+
+  int mean_size = total_size / task_count;
+  int var_size = (total_size_sq / task_count) - (mean_size * mean_size);
+  int mean_time = total_time / task_count;
+  int var_time = (total_time_sq / task_count) - (mean_time * mean_time);
+
+  {
+    std::lock_guard<std::mutex> lock(outsmtx);
+    outs() << "\nThread " << tid << "\ttime:\t" << duration.count() << " ms\n";
+    outs() << "Max task time :\t " << max_time << " ms with\t " << max_size
+           << " BBs\n";
+    outs() << "Tasks processed:\t" << task_count << "\n";
+    outs() << "Task size mean:\t" << mean_size << ", var:\t" << var_size
+           << ", std dev:\t" << (int)std::sqrt(var_size) << "\n";
+    outs() << "Task time mean:\t" << mean_time << ", var:\t" << var_time
+           << ", std dev:\t" << (int)std::sqrt(var_time) << "\n";
+  }
+#endif
 }
 
 int main(int argc, char *argv[]) {
@@ -155,22 +258,77 @@ int main(int argc, char *argv[]) {
   outs() << module->getFunctionList().size() << " function(s)\n";
   auto start = std::chrono::high_resolution_clock::now();
 
+#ifndef NTHREADS
+#define NTHREADS 16
+#endif
+
+// #define CONCURRENT
+#ifdef CONCURRENT
+  outs() << "Concurrent mode\n";
+  std::priority_queue<TaskInfo> taskQ;
+  for (auto [i, func] : enumerate(*module)) {
+    if (func.isDeclaration())
+      continue;
+    taskQ.push({&func, func.size(), (int)i});
+  }
+  std::mutex Qmutex;
+  std::vector<std::thread> threads;
+  threads.reserve(NTHREADS);
+  for (int i = 0; i < NTHREADS; ++i) {
+    threads.emplace_back(threadedPoints2, std::ref(Qmutex), std::ref(taskQ), i);
+  }
+  for (auto &t : threads) {
+    t.join();
+  }
+
+#else
+  outs() << "Sequential mode\n";
+
+// #define CSV
+#ifdef CSV
+  std::string csvname = std::string(argv[1]) + ".csv";
+  std::ofstream csv(csvname);
+  csv << "name,size,inum,time(us)\n";
+#ifndef RUN_COUNT
+#define RUN_COUNT 1
+#endif
+#endif
+
   for (auto &func : *module) {
     if (func.isDeclaration())
       continue;
-
-    pt.clear();
-    PFG.clear();
-
-    initialize(func);
-    solve();
+#ifdef CSV
+    std::string fname = func.getName().str();
+    size_t fsize = func.size();
+    int instNum = 0;
+    for (BasicBlock &BB : func) {
+      instNum += BB.size();
+    }
+    int tftime = 0;
+    for (int r = 0; r < RUN_COUNT; ++r) {
+      auto fstart = std::chrono::high_resolution_clock::now();
+#endif
+      LocalData localdata;
+      initialize(func, localdata);
+      solve(localdata);
+#ifdef CSV
+      auto fend = std::chrono::high_resolution_clock::now();
+      auto ftime =
+          std::chrono::duration_cast<std::chrono::microseconds>(fend - fstart)
+              .count();
+      tftime += ftime;
+    }
+    tftime /= RUN_COUNT;
+    csv << fname << "," << fsize << "," << instNum << "," << tftime << "\n";
+#endif
 
 #ifdef PRINT_RESULTS
     outs() << "\nFunction: " << func.getName() << "\n";
-    print();
+    print(localdata);
     outs() << "******************************** " << func.getName() << "\n";
 #endif
   }
+#endif
 
   auto end = std::chrono::high_resolution_clock::now();
   auto duration =
